075445ec59344e9ee6247ff6dceb4b93736f41f5
Ignacio Gil <nachogilgarzon@gmail.com>
2021-01-18 19:33:16 +0100

    commit 2

--- IgnacioGil_AndrésPérez/src/IgnacioGil_AndrésPérez.java
+++ IgnacioGil_AndrésPérez/src/IgnacioGil_AndrésPérez.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
@@ -6,8 +7,6 @@ public class IgnacioGil_AndrésPérez {
 
 	public static int matriz_a_CSV (String[][] inventario, String ruta) {
 
-		//COMMIT DE PRUEBA
-		System.out.println("No se puede abrir el fichero");
 		int num_lineas = 0;
 		try {
 			FileWriter fichero = new FileWriter(ruta);
@@ -28,21 +27,17 @@ public class IgnacioGil_AndrésPérez {
 		return num_lineas;
 	}
 
-	public static int imprimir_menu () {
-		int opcion = 0;
+	public static int menuPrincipal () {
 		Scanner teclado = new Scanner(System.in);
+		int opcion = 0;
 
 		System.out.print(
 				"Bienvenido al Proyecto de Programación nº3: Calles limpias de nieve \n"+
 				"\n-------- Elija el municipio sobre el que quiere trabajar --------" + "\n" +
 				"  0 - Salir" + "\n" +
-				"  1 - Madrid centro" + "\n" +
-				"  2 - Villaviciosa de Odón" + "\n" +
-				"  3 - Las Rozas" + "\n" +
-				"  4 - Las Matas" + "\n" +
-				"  5 - Torrelodones" + "\n" +
-				"  6 - Borrar item" + "\n" +
-				"  7 - Guardar datos actuales en inventario.csv" + "\n"+
+				"  1 - Agregar Municipio" + "\n" +
+				"  2 - Cargar y Editar Municipio" + "\n" +
+				"  3 - Eliminar Municipio" + "\n" +
 
 				"Opción: "
 		);
@@ -56,11 +51,99 @@ public class IgnacioGil_AndrésPérez {
 		return opcion;
 	}
 
+	public static int menuEdicion () {
+		Scanner teclado = new Scanner(System.in);
+		int opcion = 0;
+
+		System.out.print(
+				"\n-------- Elija la opción --------" + "\n" +
+				"  0 - Salir" + "\n" +
+				"  1 - Agregar Municipio" + "\n" +
+				"  2 - Cargar y Editar Municipio" + "\n" +
+				"  3 - Eliminar Municipio" + "\n" +
+
+				"Opción: "
+		);
+
+		while (!teclado.hasNextInt()) {
+			System.out.println("Opcion incorrecta");
+			teclado.next();
+		}
+		opcion = teclado.nextInt();
+
+		return opcion;
+	}
+
+	public static void nuevo_dataframe() {
+
+		Scanner teclado = new Scanner(System.in);
+        System.out.println("Nombre del fichero nuevo: ");
+        teclado = new Scanner(System.in);
+        while (!teclado.hasNext()) {
+            System.out.println("Ingresa un nombre correcto!");
+            teclado.nextLine();
+        }
+        String path_dataframe;
+        path_dataframe = teclado.nextLine();
+        File fichero = new File(path_dataframe);
+        boolean resultado;
+        try {
+            resultado = fichero.createNewFile();
+            if (resultado) {
+                System.out.println("Se ha creado un nuevo fichero en: " + fichero.getCanonicalPath());
+            } else {
+                System.out.println("Fichero ya existe!");
+            }
+
+        } catch (IOException e) {
+            System.out.println("Ha Ocurrido un  Error inesperado");
+
+        }
+    }
+
+	// Funcion: Cargar DataFrame existente.
+    public static void cargar_fichero(String path_fichero) {
+
+    	Scanner teclado = new Scanner(System.in);
+        System.out.println("\nNombre del fichero a cargar: ");
+        teclado = new Scanner(System.in);
+        while (!teclado.hasNext()) {
+            System.out.println("Ingresa un nombre correcto!");
+            teclado.nextLine();
+        }
+        path_fichero = teclado.nextLine();
+        File fichero = new File(path_fichero);
+        try {
+            Scanner leer = new Scanner(fichero);
+
+            while (leer.hasNextLine()) {
+                System.out.println(" " + leer.nextLine());
+            }
+            leer.close();
+            System.out.println("Fichero cargado con exito!");
+
+        } catch (IOException e) {
+            System.out.println("Error: Fichero no encontrado!");
+        }
+    }
+
+    // Funcion Auxiliar
+    public static void mostrar_matriz(String[][] Matrix) {
+        System.out.print("Datos agregados: \n");
+        System.out.println(" ");
+        for (int t = 0; t < Matrix.length; t++) {
+            for (int d = 0; d < Matrix[0].length; d++) {
+                System.out.print(Matrix[t][d]);
+            }
+            System.out.println("");
+        }
+    }
+
 	public static void main(String[] args) {
 		int opcion = -1;
 
 		while (opcion != 0) {
-			opcion = imprimir_menu();
+			opcion = menuPrincipal();
 			switch (opcion) {
 			case 1:
 				break;
